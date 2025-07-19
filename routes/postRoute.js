3064f4fe7fa44c1b4459642af2aa35f3aecd81de
Rohit vishwakarma <rohitvvishwakarma400@email.com>
2025-07-19 10:34:58 -0400

    modified schema

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-// import  { protect } from '../middleware/authMiddleware.js'
+import  { protect } from '../middleware/authMiddleware.js'
 import { createPost, deletePost, getAllPosts, getSinglePost, updatePost } from '../controllers/postController.js'
 
 const router = Router()
 
-router.post('/posts', createPost);
-router.get('/posts', getAllPosts);
-router.get('/posts/:id', getSinglePost);
+router.post('/posts',protect, createPost);
+router.get('/posts',protect, getAllPosts);
+router.get('/posts/:id',protect, getSinglePost);
 router.put('/posts/:id', updatePost);
 router.delete('/posts/:id', deletePost);
 
