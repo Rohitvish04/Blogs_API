ce4b4481430cda2c294b255deb3b2ef04db9ec1a
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2025-08-17 05:48:25 -0400

    Update server.js
    cors add origin

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Middleware
-app.use(cors());
+app.use(cors({  
+  origin: process.env.ORIGIN,
+  credentials: true,               
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
