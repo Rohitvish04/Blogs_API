98b39cfa5d699136d77ed9271459918a069b597c
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2025-09-10 16:15:59 +0000

    add author name in comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -10,6 +10,7 @@ export const createComment = async (req, res) => {
                  content,
                  parentId: parentId ? Number(parentId) : null,
             },
+            include: { author: { select: { name: true } } } // Include author's name
         });
         res.status(201).json(comment);
     } catch (err) {
@@ -19,8 +20,10 @@ export const createComment = async (req, res) => {
 
 export const getcomment = async (req, res) => {
     try {
-    const comment = await prisma.comment.findMany();
-    res.status(200).json(comment);
+        const comment = await prisma.comment.findMany({
+            include: { author: { select: { name: true } } } // Include author's name
+        });
+        res.status(200).json(comment);
     } catch (err) {
         res.status(500).json({ message: "server error", error: err.message });
     }
