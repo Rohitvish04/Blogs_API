a45a9b1d0b84fb397063d4761f108250c6af4487
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2026-01-28 18:54:37 +0530

    Implement normalizeGender function in authController
    Add normalizeGender function to standardize gender input.

--- controllers/authController.js
+++ controllers/authController.js
@@ -108,6 +108,11 @@ export const updateProfile = async (req, res) => {
         //     avatarUrl = "https://images.unsplash.com/photo-1757573913927-0f6a58fb0f49?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
         // }
 
+            const normalizeGender = (gender) => {
+                if (!gender) return undefined;
+                return gender.toUpperCase();
+            };
+
             //Build update object dynamically
             const updateData = {
                 ...(name && { name }),
